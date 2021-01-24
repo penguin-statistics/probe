16bb3d49f85d0a264df7bba9258b7ed9cd17909e
GalvinGao <galvingaoper@gmail.com>
2021-01-24 10:58:14 +0800

    remove redundant

--- internal/app/controller/bonjour.go
+++ internal/app/controller/bonjour.go
@@ -135,10 +135,6 @@ func (bc *Bonjour) LiveHandler(ctx echo.Context) error {
 		return nil
 	}
 
-	defer func() {
-		log.Println("BYE FROM HTTP!!!")
-	}()
-
 	bc.hub.Register <- client
 	go client.Read()
 	go client.Write()
