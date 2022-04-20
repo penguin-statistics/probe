3b7a5bf4d0dae7f3d48d9f3f9e075109ff3970fc
GalvinGao <me@galvingao.com>
2022-04-20 21:29:04 +0800

    feat: improve route cleaning

--- internal/pkg/commons/clientRoutes.go
+++ internal/pkg/commons/clientRoutes.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"net/url"
 	"strings"
+	"unicode"
 )
 
 // CleanClientRoute is to verify if a path passed from client is a valid endpoint and removes query strings or other
@@ -16,6 +17,14 @@ func CleanClientRoute(route string) (string, error) {
 	// the route (or a path) shall be absolute and doesn't have the Host and Scheme set
 	if len(r.Path) > 128 || !strings.HasPrefix(r.Path, "/") {
 		return "(malformed)", errors.New("malformed route")
+	}
+	// if there's any non-ascii character
+	// then remove everything after the first non-ascii character
+	// and return the cleaned route
+	for i, c := range r.Path {
+		if !unicode.IsPrint(c) {
+			return r.Path[:i], nil
+		}
 	}
 	return r.Path, nil
 }
